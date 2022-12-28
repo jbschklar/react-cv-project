15331e6706ce688cf442e28912a2cd8e603b3ef0
jbschklar <jbschklar@gmail.com>
2022-12-28 15:27:43 -0500

    refactored to allow render of exisiting list of schools and form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import PersonalData from "./components/PersonalData";
-import Education from "./components/Education";
+import EducationForm from "./components/Education";
+import uniqid from "uniqid";
 
 class App extends Component {
 	constructor(props) {
@@ -53,6 +54,7 @@ class App extends Component {
 				school: obj.school,
 				major: obj.major,
 				gradDate: obj.gradDate,
+				id: uniqid(),
 			}),
 		});
 	}
@@ -61,7 +63,10 @@ class App extends Component {
 		return (
 			<div>
 				<PersonalData handleSubmit={this.handleSubmit} obj={this.state} />
-				<Education handleSubmit={this.handleEduSubmit} obj={this.state} />
+				{
+					// refactor to render Education for each item in education array if length > 0 that can be edited and a new form at the end of the list  //
+				}
+				<EducationForm handleSubmit={this.handleEduSubmit} obj={this.state} />
 			</div>
 		);
 	}
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
 import { MdModeEditOutline } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
 
-class Education extends Component {
+const EducationList = (props) => {
+	// const {edit, id, editID, value} = props;
+	// if(edit && id === editID) {
+	// 	return <input type='text' placeholder={value} />;
+	// }
+
+	if (props.obj.education.length > 0)
+		return (
+			<ul>
+				{props.obj.education.map((el) => {
+					return (
+						<li key={el.id} id={el.id}>
+							<h3>School: {el.school}</h3>
+							<h3>Major: {el.major}</h3>
+							<h3>Graduation: {el.gradDate}</h3>
+							{/* <EditIcon
+								editFn={editItem}
+								submitFn={() => {
+									this.props.handleSubmit(this.props.obj.handleSubmit);
+									this.props.setState({
+										edit: false,
+									});
+								}}
+							/> */}
+						</li>
+					);
+				})}
+			</ul>
+		);
+};
+
+const EditIcon = (props) => {
+	return (
+		<div>
+			<MdModeEditOutline onClick={props.editFn} />
+			<BsCheck onClick={props.submitFn} className="hidden" />
+		</div>
+	);
+};
+
+class EducationForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -9,8 +50,16 @@ class Education extends Component {
 			school: null,
 			major: null,
 			gradDate: null,
+			id: "",
 		};
+		this.editItem = this.editItem.bind(this);
 	}
+
+	editItem = (e) => {
+		this.setState({
+			edit: true,
+		});
+	};
 
 	handleNameChange = (e) => {
 		this.setState({
@@ -41,68 +90,96 @@ class Education extends Component {
 	render() {
 		if (this.state.edit) {
 			return (
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						if (!this.validateSubmit(this.state)) return;
+				<div>
+					<EducationList obj={this.props.obj} />
+
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							if (!this.validateSubmit(this.state)) return;
+							this.setState({
+								edit: false,
+							});
+							this.props.handleSubmit(this.state);
+						}}
+					>
+						<label>
+							School Name:
+							<input
+								type="text"
+								className="school-name"
+								onChange={this.handleNameChange}
+							></input>
+						</label>
+						<label>
+							Major:
+							<input
+								type="text"
+								className="major"
+								onChange={this.handleMajorChange}
+							></input>
+						</label>
+						<label>
+							Graduation Year:
+							<input
+								type="number"
+								min="1900"
+								max="2099"
+								className="grad-year"
+								onChange={this.handleGradChange}
+							></input>
+						</label>
+						<button type="submit">Submit</button>
+					</form>
+					<EditIcon
+						editFn={this.editItem}
+						submitFn={() => {
+							this.props.handleSubmit(this.state);
+							this.setState({
+								edit: false,
+							});
+						}}
+					/>
+				</div>
+			);
+		}
+		return (
+			<div>
+				<EducationList obj={this.props.obj} />
+				<EditIcon
+					editFn={this.editItem}
+					submitFn={() => {
+						this.props.handleSubmit(this.state);
 						this.setState({
 							edit: false,
 						});
-						this.props.handleSubmit(this.state);
 					}}
-				>
-					<label>
-						School Name:
-						<input
-							type="text"
-							className="school-name"
-							onChange={this.handleNameChange}
-						></input>
-					</label>
-					<label>
-						Major:
-						<input
-							type="text"
-							className="major"
-							onChange={this.handleMajorChange}
-						></input>
-					</label>
-					<label>
-						Graduation Year:
-						<input
-							type="number"
-							min="1900"
-							max="2099"
-							className="grad-year"
-							onChange={this.handleGradChange}
-						></input>
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-			);
-		}
-
-		return (
-			<ul>
-				{this.props.obj.education.map((el) => {
-					return (
-						<li>
-							<h3>School: {el.school}</h3>
-							<h3>Major: {el.major}</h3>
-							<h3>Graduation: {el.gradDate}</h3>
-							<MdModeEditOutline
-								onClick={(e) => {
-									this.setState({
-										edit: true,
-									});
-								}}
-							/>
-						</li>
-					);
-				})}
-			</ul>
+				/>
+			</div>
 		);
+		// return (
+		// 	<ul>
+		// 		{this.props.obj.education.map((el) => {
+		// 			return (
+		// 				<li key={el.id} id={el.id}>
+		// 					<h3>School: {el.school}</h3>
+		// 					<h3>Major: {el.major}</h3>
+		// 					<h3>Graduation: {el.gradDate}</h3>
+		// 					<EditIcon
+		// 						editFn={this.editItem}
+		// 						submitFn={() => {
+		// 							this.props.handleSubmit(this.state);
+		// 							this.setState({
+		// 								edit: false,
+		// 							});
+		// 						}}
+		// 					/>
+		// 				</li>
+		// 			);
+		// 		})}
+		// 	</ul>
+		// );
 	}
 }
 
-export default Education;
+export default EducationForm;
